5f0fb9f0d0c7b7464617086a6f74ab35039905af
RodrigoAroeira <raroeirabr@gmail.com>
2023-04-07 18:22:30 -0300

    Dabliu

--- Python/Condicionais/Problema5.py
+++ Python/Condicionais/Problema5.py
@@ -1,8 +1,8 @@
-import math
+from math import sqrt
 
 num = float(input("Digite um número: "))
 
 if num >= 0:
-    print (f"Resultado: {math.sqrt(num):.3f}")
+    print (f"Resultado: {sqrt(num):.3f}")
 else:
     print ("Número inválido")
