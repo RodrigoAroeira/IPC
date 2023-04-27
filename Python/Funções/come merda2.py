6899255eb779739df403ad6218aff8b7e0191fa7
RodrigoAroeira <raroeirabr@gmail.com>
2023-04-27 20:47:07 -0300

    come merda

--- Python/Funções/come merda2.py
+++ Python/Funções/come merda2.py
@@ -2,14 +2,12 @@ from unidecode import unidecode
 def come_merda():
     while True:
         frase = input("Você está bem? Sim ou não: ")
-        if unidecode(frase) == 'nao':
+        if unidecode(frase.lower()) == 'nao':
             print("Vontade de come merda.")
             break
         else:
             continue
 
-def main():
-    input ("Pressione enter para sofrer.")
-    come_merda()
+input ("Pressione enter para sofrer.")
+come_merda()
 
-main()
