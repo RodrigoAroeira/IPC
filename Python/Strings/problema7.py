a0d86ec946fa49395c377afb87a05d27c831d804
RodrigoAroeira <raroeirabr@gmail.com>
2023-05-08 21:27:49 -0300

    Problema strings

--- Python/Strings/problema7.py
+++ Python/Strings/problema7.py
@@ -1,9 +1,26 @@
-palavra = input()
-chave = int(input())
+palavra = input() 
+chave = int(input()) 
 
-def cifra(palavra, chave, lc = []):
-    for char in palavra.lower():
-        lc.append(chr(ord(char.lower()) + chave))
-    print("".join(lc))
+def cifra(palavra, chave, lc = []): # método 1
+    for char in palavra.lower(): 
+        if ord(char.lower()) + chave <= 122:
+            lc.append(chr(ord(char.lower()) + chave)) 
+        else: 
+            lc.append(chr(ord(char.lower()) + chave - 26)) 
+    x = "".join(lc) 
+    print("Resultado: " + x) 
+# cifra(palavra, chave) 
 
-cifra(palavra, chave)
+def cifra_cesar(palavra, chave): #método 2
+    criptografado = ""
+    for letra in palavra:
+        if ord(letra) + chave <= 122:
+            nova_letra = chr((ord(letra) + chave))
+        else:
+            nova_letra = chr((ord(letra) + chave) - 26)
+        criptografado += nova_letra
+    print("Resultado:", criptografado)
+cifra_cesar(palavra, chave)
+
+
+
