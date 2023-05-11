7853f9e277620b00984fba1dabfa71d15f6136b0
RodrigoAroeira <raroeirabr@gmail.com>
2023-05-11 10:20:49 -0300

    função de jeito mais certo

--- Python/Listas/problema7.py
+++ Python/Listas/problema7.py
@@ -2,7 +2,7 @@ def nested_sum(lista, l=[]):
     if lista != []:
         for i in lista:
             if type(i) == list:
-                nested_sum(i)
+                nested_sum(i, l)
             else:
                 l.append(i)
         return sum(l)
