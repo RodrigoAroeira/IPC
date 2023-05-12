0d2f0bc81f3dcbc47bb1d8a85e3d4344e775c814
RodrigoAroeira <raroeirabr@gmail.com>
2023-05-12 20:05:07 -0300

    Teste para rodar o programa comentado

--- Python/Listas/problema7.py
+++ Python/Listas/problema7.py
@@ -1,5 +1,5 @@
 def nested_sum(lista, l=[]):
-    if lista != []:
+    if lista:
         for i in lista:
             if type(i) == list:
                 nested_sum(i, l)
@@ -8,3 +8,7 @@ def nested_sum(lista, l=[]):
         return sum(l)
     else:
         return 0
+
+# lista = [1, 2, [3], [4, [5, 6], 7], 8, [9, [10]]]
+
+# print(nested_sum(lista))
