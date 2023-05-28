3c6d4fb5f1c4d4c58a18232cac4ee9a6b27953c5
RodrigoAroeira <raroeirabr@gmail.com>
2023-05-28 19:21:42 -0300

    Aprimoramento e eficientizaçao do codigo

--- Python/Avaliações/Avaliação 2/problema1.py
+++ Python/Avaliações/Avaliação 2/problema1.py
@@ -25,22 +25,33 @@ def compactar_strings(string1, string2, compactado = []):
         string1 += " " * (len(string2) - len(string1))
     for i in range(len(string1)):
         compactado.append([string1[i], string2[i]])
+    for elemento in compactado:
+        if elemento[0] == " ":
+            elemento[0] = ""
+        if elemento[1] == " ":
+            elemento[1] = ""
     return compactado
 
 def intercalar_strings(string1, string2, intercalado = ''):
-    if len(string1) > len(string2):
-        for i in range(len(string1)):
-            if i < len(string2):
-                intercalado += string1[i] + string2[i]
-            else:
-                intercalado += string1[i]
-    else:
-        for i in range(len(string2)):
-            if i < len(string1):
-                intercalado += string1[i] + string2[i]
-            else:
-                intercalado += string2[i]
+    compactado = compactar_strings(string1, string2)
+    for i in range(len(compactado)//2):
+        intercalado += compactado[i][0] + compactado[i][1]
     return intercalado
+
+
+    # if len(string1) > len(string2):
+    #     for i in range(len(string1)):
+    #         if i < len(string2):
+    #             intercalado += string1[i] + string2[i]
+    #         else:
+    #             intercalado += string1[i]
+    # else:
+    #     for i in range(len(string2)):
+    #         if i < len(string1):
+    #             intercalado += string1[i] + string2[i]
+    #         else:
+    #             intercalado += string2[i]
+    # return intercalado
 
 print(compactar_strings("Tpo", "oCder"))
 print(intercalar_strings("Tpo", "oCder")) # output deve ser TopCoder
