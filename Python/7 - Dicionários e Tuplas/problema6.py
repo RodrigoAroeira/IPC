dcd96e2c308df0cff4790e0a32bddca07010f683
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-01 14:39:01 -0300

    Refeito corretamente

--- Python/7 - Dicionários e Tuplas/problema6.py
+++ Python/7 - Dicionários e Tuplas/problema6.py
@@ -7,9 +7,10 @@ DEZENAS = {0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL", 5: "L", 6: "LX", 7: "LXX",
 CENTENAS = {0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD", 5: "D", 6: "DC", 7:
 "DCC", 8: "DCCC", 9: "CM"}
 
-num = [*int(input())].reverse()
+num = [*(input())]
+num = list(map(int, num))
 
-if len(num) ==1:
+if len(num) == 1:
     print(UNIDADES[(num[0])])
 elif len(num) == 2:
     print(DEZENAS[(num[0])]+UNIDADES[(num[1])])
