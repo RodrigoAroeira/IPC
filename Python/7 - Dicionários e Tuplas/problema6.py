a33402390acb33267d2b89a27153962e17f786f8
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-03 18:04:24 -0300

    Dicionários e Tuplas

--- Python/7 - Dicionários e Tuplas/problema6.py
+++ Python/7 - Dicionários e Tuplas/problema6.py
@@ -1,19 +1,19 @@
-UNIDADES = {0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8:
+unidades = {0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8:
 "VIII", 9: "IX"}
 
-DEZENAS = {0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL", 5: "L", 6: "LX", 7: "LXX",
+dezenas = {0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL", 5: "L", 6: "LX", 7: "LXX",
 8: "LXXX", 9: "XC"}
 
-CENTENAS = {0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD", 5: "D", 6: "DC", 7:
+centenas = {0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD", 5: "D", 6: "DC", 7:
 "DCC", 8: "DCCC", 9: "CM"}
 
 num = [*(input())]
 num = list(map(int, num))
 
 if len(num) == 1:
-    print(UNIDADES[(num[0])])
+    print(unidades[(num[0])])
 elif len(num) == 2:
-    print(DEZENAS[(num[0])]+UNIDADES[(num[1])])
+    print(dezenas[(num[0])]+unidades[(num[1])])
 else:
-    print(CENTENAS[(num[0])]+DEZENAS[(num[1])]+UNIDADES[(num[2])])
+    print(centenas[(num[0])]+dezenas[(num[1])]+unidades[(num[2])])
 
