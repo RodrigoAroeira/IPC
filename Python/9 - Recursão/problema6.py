ed7ad8f4c64fd45e43b969180ed8404d9a2b70e4
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-08 14:37:33 -0300

    Recursão

--- Python/9 - Recursão/problema6.py
+++ Python/9 - Recursão/problema6.py
@@ -1,2 +1,18 @@
-def imprime_naturais(n):
-    pass
+def imprime_naturais(n, numeros = []):
+    [numeros.append(n) if n % 2 == 0 else None]
+    [imprime_naturais(n-1, numeros) if n > 0 else None]
+    return numeros[::-1]
+
+n = int(input())
+numeros = imprime_naturais(n)
+for numero in numeros:
+    print(numero)
+
+# def imprime_naturais(n):
+#     if n % 2 == 0:
+#         print(n)
+#     if n > 0:
+#         imprime_naturais(n-1)
+
+# n = int(input())
+# imprime_naturais(n)
