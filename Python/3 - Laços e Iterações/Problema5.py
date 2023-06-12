bbafea4ad4c89408426473a7a9f59d9c3f9cba3d
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-12 01:20:01 -0300

    Funny

--- Python/3 - Laços e Iterações/Problema5.py
+++ Python/3 - Laços e Iterações/Problema5.py
@@ -6,4 +6,6 @@ pares = 0
 for i in range(start, end+1):
     if i % 2 == 0:
         pares += 1
+pares2 = (end+1 - start)
 print (f"Quantidade de números pares: {pares}")
+print(((pares2/2) if (pares2-1) % 2 == 1 else (pares2-1)/2))
