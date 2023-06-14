d2deb1597a47fe1b4ddf2790fec8b8b589002a32
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-14 14:26:38 -0300

    a

--- Python/Avaliações/Avaliação 3/problema2.py
+++ Python/Avaliações/Avaliação 3/problema2.py
@@ -10,5 +10,3 @@ def incluir_aluno(path, nome, listanotas):
     with open(path, 'a') as arquivo:
         for aluno, notas in alunos.items():
             arquivo.write(f'{aluno},{av1},{av2},{av3},{av4}\n')
-
-incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\notas.txt", 'Rodrigo', [10, 100, 1, 2])
