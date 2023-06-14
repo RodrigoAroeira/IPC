a6725ec0bc93e27eb50ed73d9f69d501ea0484b6
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-14 14:07:08 -0300

    a

--- Python/Avaliações/Avaliação 3/problema4.py
+++ Python/Avaliações/Avaliação 3/problema4.py
@@ -1,4 +1,8 @@
 def busca_binaria(lista, elemento):
+    if elemento not in lista:
+        return False
+    if len(lista) == 0:
+        return False
     if len(lista) % 2 == 0:
         meio = len(lista) // 2
     else:
@@ -8,4 +12,6 @@ def busca_binaria(lista, elemento):
     else:
         lista.remove(lista[meio])
         return busca_binaria(lista, elemento)
-        
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(busca_binaria(lista, 9))
