ec9fed9e890b6a91798bb07e86fb87b61cac13c8
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-14 14:20:28 -0300

    a

--- Python/Avaliações/Avaliação 3/problema2.py
+++ Python/Avaliações/Avaliação 3/problema2.py
@@ -2,13 +2,14 @@ def incluir_aluno(path, nome, listanotas):
     tamanho = len(listanotas)
     alunos = {}
     for nota in listanotas:
-        if any(notas < 0 or notas > 25 for notas in listanotas):
+        if any(notas < 0 or notas > 25 for notas in listanotas) == True:
             pass
-        elif all(notas >= 0 and notas <= 25 for notas in listanotas):
+        elif all(notas >= 0 and notas <= 25 for notas in listanotas) == True:
             alunos[nome] = listanotas
+    av1, av2, av3, av4 = listanotas    
     with open(path, 'a') as arquivo:
         for aluno, notas in alunos.items():
-            for av1, av2, av3, av4 in notas:
+            if aluno not in arquivo:
                 arquivo.write(f'{aluno},{av1},{av2},{av3},{av4}\n')
 
 incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\notas.txt", 'Rodrigo', [10, 10, 1, 2])
