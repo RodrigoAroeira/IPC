3d24be5344dc1838032d7711455f4ee0a3306e3f
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-14 14:03:23 -0300

    a

--- Python/Avaliações/Avaliação 3/problema4.py
+++ Python/Avaliações/Avaliação 3/problema4.py
@@ -1,17 +1,11 @@
 def busca_binaria(lista, elemento):
-    inicio = 0
-    fim = len(lista) - 1
     if len(lista) % 2 == 0:
         meio = len(lista) // 2
     else:
-        meio = (len(lista) + 1) // 2
-    valor = int(input("Digite o valor a ser encontrado: "))
-    while inicio <= fim:
-        if lista[meio] == valor:
-            return meio
-        elif lista[meio] < valor:
-            inicio = meio + 1
-        else:
-            fim = meio - 1
-        meio = (inicio + fim) // 2
-    return -1
+        meio = (len(lista) // 2) + 1
+    if lista[meio] == elemento:
+        return True
+    else:
+        lista.remove(lista[meio])
+        return busca_binaria(lista, elemento)
+        
