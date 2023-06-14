097eae42dfdb408e71a5b1582f6b8f7ff287cb93
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-14 14:23:50 -0300

    a

--- Python/Avaliações/Avaliação 3/problema2.py
+++ Python/Avaliações/Avaliação 3/problema2.py
@@ -3,7 +3,7 @@ def incluir_aluno(path, nome, listanotas):
     alunos = {}
     for nota in listanotas:
         if any(notas < 0 or notas > 25 for notas in listanotas) == True:
-            pass
+            return False
         elif all(notas >= 0 and notas <= 25 for notas in listanotas) == True:
             alunos[nome] = listanotas
             av1, av2, av3, av4 = listanotas    
