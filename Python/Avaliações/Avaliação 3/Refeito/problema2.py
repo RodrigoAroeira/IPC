32c923c1db90f69593341ca74bb3fd7a972e94ef
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-19 01:11:45 -0300

    Avaliação 3

--- Python/Avaliações/Avaliação 3/Refeito/problema2.py
+++ Python/Avaliações/Avaliação 3/Refeito/problema2.py
@@ -1,4 +1,4 @@
-def incluir_aluno(path, nome, listanotas):
+def incluir_aluno(path, nome, listanotas, a=0):
     alunos = {}
     if any(notas < 0 or notas > 25 for notas in listanotas):
         return False
@@ -6,9 +6,18 @@ def incluir_aluno(path, nome, listanotas):
         alunos[nome] = listanotas
         av1, av2, av3, av4 = listanotas    
     with open(path, 'a') as arquivo:
-        for aluno, _ in alunos.items():
-            arquivo.write(f'{aluno},{av1},{av2},{av3},{av4}\n') 
+        if a == 1:
+            arquivo.truncate(0)
+            arquivo.close()
+        try:
+            for aluno, _ in alunos.items():
+                arquivo.write(f'{aluno},{av1},{av2},{av3},{av4}\n')
+        except: 
+            from os import system as s; s('cls')
+            exit()
     return True
 
-notas = [10, 10, 10, 25]
-print(incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\Refeito\notas.txt", "Fezes", notas))
+a = 1
+
+notas = [0, 0, 10, 25]
+print(incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\Refeito\notas.txt", "Fezes", notas, a))
