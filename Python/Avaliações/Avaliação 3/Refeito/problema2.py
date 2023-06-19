e546e5005c096e84d7c1c6b25e17c95938db0749
RodrigoAroeira <raroeirabr@gmail.com>
2023-06-19 13:51:47 -0300

    Avaliação 3

--- Python/Avaliações/Avaliação 3/Refeito/problema2.py
+++ Python/Avaliações/Avaliação 3/Refeito/problema2.py
@@ -1,4 +1,4 @@
-def incluir_aluno(path, nome, listanotas, a=0):
+def incluir_aluno(path, nome, listanotas, var=0):
     alunos = {}
     if any(notas < 0 or notas > 25 for notas in listanotas):
         return False
@@ -6,7 +6,7 @@ def incluir_aluno(path, nome, listanotas, a=0):
         alunos[nome] = listanotas
         av1, av2, av3, av4 = listanotas    
     with open(path, 'a') as arquivo:
-        if a == 1:
+        if var == 1:
             arquivo.truncate(0)
             arquivo.close()
         try:
@@ -17,7 +17,7 @@ def incluir_aluno(path, nome, listanotas, a=0):
             exit()
     return True
 
-a = 1
+var = 1
 
 notas = [0, 0, 10, 25]
-print(incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\Refeito\notas.txt", "Fezes", notas, a))
+print(incluir_aluno(r"C:\Users\Rodrigo\OneDrive - Universidade Federal de Minas Gerais\Documentos\Programação\IPC\Python\Avaliações\Avaliação 3\Refeito\notas.txt", "Fezes", notas, var))
